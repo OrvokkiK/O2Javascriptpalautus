8d69e8c28b30106dd51a2fec3a1c7ef18b4ccdf8
OrvokkiK <orvokki.kettula@metropolia.fi>
2023-03-30 14:38:28 +0300

    Moduuli 1 Tehtävä 3

--- Module1/M1T3.js
+++ Module1/M1T3.js
@@ -7,8 +7,6 @@ let summa = x + y + z;
 let tulo = x * y * z;
 let ka = summa / 3;
 
-document.querySelector('#summa').innerHTML =
-
 document.querySelector('#summa').innerHTML = 'Summa:' + summa;
 document.querySelector('#tulo').innerHTML = 'Tulo:' + tulo;
 document.querySelector('#ka').innerHTML = 'keskiarvo:' + ka;
