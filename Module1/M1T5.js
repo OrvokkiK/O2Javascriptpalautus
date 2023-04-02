da4291b3195c20752a9f32339deca67afac9ffae
OrvokkiK <orvokki.kettula@metropolia.fi>
2023-04-02 16:54:47 +0300

    Moduuli 1 Tehtävä 5

--- Module1/M1T5.js
+++ Module1/M1T5.js
@@ -1,25 +1,11 @@
 'use strict';
-const year = parseInt(prompt("Anna vuosiluku:"))
-let leap = false;
-
-if (year < 400) {
-    if (year % 4 == 0) {
-        leap = true;
+const year = parseInt(prompt("Enter a year"));
+function leap_check(year) {
+    if ((0 === year % 4) && (0 !== year % 100) || (0 === year % 400)) {
+       document.querySelector('#year').innerHTML = year + ' is a leap year.';
+    } else {
+       document.querySelector('#year').innerHTML = year + ' is not a leap year.';
     }
-    if (year % 100 == 0 && year % 400 == 0){
-        leap = true;
-    }}
-else {
-    leap = false
-}
-if (leap == false) {
-    document.querySelector('#year').innerHTML = year + 'is not a leap year.';
 }
 
-if (leap == true) {
-    document.querySelector('#year').innerHTML = year + 'is a leap year.';
-}
-
-else {
-   document.querySelector('#year').innerHTML = 'An Error has occured';
-}
+leap_check(year);
