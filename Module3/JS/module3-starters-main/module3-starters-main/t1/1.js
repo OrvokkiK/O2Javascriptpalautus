3c8e55f42c352fe687715b0016512285b23b688c
OrvokkiK <orvokki.kettula@metropolia.fi>
2023-04-13 00:18:51 +0300

    Moduuli 3 Tehtävä 1

---
+++ Module3/JS/module3-starters-main/module3-starters-main/t1/1.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const target = document.querySelector('#target');
+const html = '<li>First item</li>'
+    + '<li>Second item</li>'
+    + '<li>Third item</li>';
+
+target.innerHTML = html;
